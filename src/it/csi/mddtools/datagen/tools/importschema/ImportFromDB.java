e080288b4c79e0934682943db84a84ee28c0a0b6
ANTONACI Mauro 869 <mauro.antonaci@csi.it>
2010-04-14 12:00:22 +0000

    tags/INTEGRAZIONE-1.0.0-001
    git-svn-id: http://subversion.csi.it:10090/workarea/mddtools/datagen.editor/trunk@1361 be04b910-fd5c-0410-891f-bca81b977169

---
+++ src/it/csi/mddtools/datagen/tools/importschema/ImportFromDB.java
@@ -0,0 +1,14 @@
+package it.csi.mddtools.datagen.tools.importschema;
+
+public class ImportFromDB {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	//public static 
+}
